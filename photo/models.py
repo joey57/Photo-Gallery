99b8966413e1ad06f17f408fec071ee2986b0a7c
joey57 <njorogejoey5@gmail.com>
2022-05-27 15:04:38 +0300

    create image model, location and category model

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.forms import ImageField
 
 # Create your models here.
+class Image(models.Model):
+  name = models.CharField(max_length=50)
+  pic = models.ImageField(upload_to = 'uploads/')
+  description = models.TextField()
+  image_location = models.ForeignKey('Location',on_delete=models.CASCADE)
+  image_category = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+class Location(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+class Category(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name  
