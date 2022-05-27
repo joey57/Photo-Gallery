cc436eb71d891fb0da42adf7ec51f572c1606fd9
joey57 <njorogejoey5@gmail.com>
2022-05-27 13:43:29 +0300

    set up base template, navbar and configure urls

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photo',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http  import HttpResponse, Http404
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+    title = 'Gallery'
+
+    return render(request, 'index.html', {'title':title})
