00fd134b0b92c2f94aa639e29572f6078cafae32
joey57 <njorogejoey5@gmail.com>
2022-05-27 16:20:53 +0300

    testing media uploads, using admin to add images

--- photo/urls.py
+++ photo/urls.py
@@ -6,3 +6,5 @@ from django.conf.urls.static import static
 urlpatterns=[
   path('', views.index , name = 'index'),
 ]
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
