1650de1aad0b7709c6cb0ee14cf21c42e472be75
joey57 <njorogejoey5@gmail.com>
2022-05-27 18:30:05 +0300

    create search template

--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   path('', views.index , name = 'index'),
+  path('search/', views.search_image, name = 'search_image'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -9,3 +9,21 @@ def index(request):
     locations = Location.objects.all()
 
     return render(request, 'index.html', {'images':images, 'images':images, 'locations':locations })
+
+def search_image(request):
+    title = 'Search'
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image_category' in request.GET and request.GET['image_category']:
+        search_term = request.GET.get('image_category')
+        found_results = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        print(search_term)
+        print(found_results)
+
+        return render(request, 'search.html', {'title':title,'images': found_results, 'message': message, 'categories': categories, 'locations':locations})
+        
+
+
+
+
