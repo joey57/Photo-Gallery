66a0e621dab9159ab5b357a9cac5dea7a7f44f28
joey57 <njorogejoey5@gmail.com>
2022-05-27 21:11:25 +0300

    adding scripts to template

--- photo/urls.py
+++ photo/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
   path('', views.index , name = 'index'),
   path('search/', views.search_image, name = 'search_image'),
   re_path('location/(?P<image_location>\d+)', views.location_filter, name = 'location_filter'),
+  re_path('image/(?P<category_name>w+)/(?P<image_id>\d+)', views.single, name = 'single'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
