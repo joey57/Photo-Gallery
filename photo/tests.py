621f267a0efe8b509868d47f424705ee0594db52
joey57 <njorogejoey5@gmail.com>
2022-05-29 14:16:09 +0300

    add search image save and get images test class

--- photo/tests.py
+++ photo/tests.py
@@ -15,7 +15,37 @@ class ImageTestClass(TestCase):
     self.image.save_image()
 
   def test_instance(self):
-    self.assertTrue(isinstance(self.image, Image))  
+    self.assertTrue(isinstance(self.image, Image))
+
+  def tearDown(self):
+    self.image.delete_image()
+    self.cat.delete_category()
+    self.loc.delete_location()   
+
+  def test_save_method(self):
+    self.image.save_image() 
+    images = Image.objects.all() 
+    self.assertTrue(len(images)> 0) 
+  
+  def test_get_image_by_id(self):
+    images = Image.get_image_by_id(self.image.id)
+    self.assertTrue(len(images) == 1)
+   
+
+  def test_filter_by_location(self):
+    images = Image.filter_by_location('1') 
+    print(images)
+    self.assertTrue(len(images) > 0)
+
+  def test_search_by_category(self):
+    images = Image.search_by_category('Fashion')
+    self.assertTrue(len(images)> 0)
+
+  def test_update_image(self):
+    image = Image.update_image(self.image.id, 'test update', 'my test', self.loc, self.cat)
+    upimage = Image.objects.filter(id = self.image.id)
+    print(upimage)
+    self.assertTrue(Image.name == 'test update')     
 
 
 class LocationTestClass(TestCase):
