9172a25efab8b498cc2db67ac0841c2a4fef7857
joey57 <njorogejoey5@gmail.com>
2022-05-29 13:54:50 +0300

    setup test for all tables

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+  #setUp method
+  def setUp(self):
+    self.image = Image(name = 'test', description = 'desc test', image_location = 'Africa', image_category = 'Fashion')
+    self.image.save_image()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.image, Image))  
+
+
+class LocationTestClass(TestCase):
+  #setUp method
+  def setUp(self):
+    self.loc = Location(name = 'Africa')
+    self.loc.save_location()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.loc, Location))  
+
+class CategoryTestClass(TestCase):
+  # set up method 
+  def setUp(self):
+    self.cat = Category(name = 'Fashion')
+    self.cat.save_category()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.cat, Category))
+
+
+
